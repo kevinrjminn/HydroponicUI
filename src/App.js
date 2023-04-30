f2d846f9dea8338f4243d9fa7032af8a8be45098
LeulMezgebe <71226608+LeulMezgebe@users.noreply.github.com>
2023-04-29 20:00:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import { Container, Typography } from '@mui/material';
 import GreenProgressBar from './components/GreenProgressBar';
-import MongoDB from './components/MongoDB';
+
 import DrawerAppBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -12,7 +12,6 @@ import TeamInfoPage from './pages/TeamInfoPage';
 
 function App() {
     const startDate = '2023-04-05';
-    const [connectionStatus, setConnectionStatus] = useState('');
 
     return (
         <Router>
@@ -26,14 +25,13 @@ function App() {
                             element={
                                 <>
                                     <Dashboard />
-                                    {/*<MongoDB setConnectionStatus={setConnectionStatus} /> */}
+                                    
                                     <Container>
                                         <Typography variant="h5" align="center" gutterBottom>
                                             Harvest progress
                                         </Typography>
                                         <GreenProgressBar startDate={startDate} />
                                         <Typography variant="h6" align="center" gutterBottom>
-                                            {connectionStatus}
                                         </Typography>
                                     </Container>
                                 </>
