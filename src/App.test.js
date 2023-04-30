4286c59d3bd078087cca67577eee3072685877d2
LeulMezgebe <71226608+LeulMezgebe@users.noreply.github.com>
2023-04-29 20:23:38 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  expect(linkElement).toBeInTheDocument();
-});
